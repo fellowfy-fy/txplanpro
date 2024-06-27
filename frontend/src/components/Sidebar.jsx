1cd2561ae0a0153510dbe993371aa9a21127023d
terrxrmachine <so3.workspace@gmail.com>
2024-06-26 23:18:56 -0400

    проработка стилей сайдбара

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,67 +3,85 @@ import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="w-64 h-screen bg-gray-100 text-gray-900 fixed flex flex-col">
+    <div className="w-64 h-screen bg-gray-100  fixed flex flex-col">
       <div className="p-6">
         <h1 className="text-2xl font-bold">TxPlanPro</h1>
       </div>
-      <p className="py-2.5 px-4">Menu</p>
       <nav className="flex-1 px-6">
-        <ul className="space-y-4">
+        <ul>
+          <p className="p-4">Menu</p>
+          <div className="pl-4 text-[18px] ">
+            <li>
+              <NavLink
+                to="/dashboard"
+                className="active:bg-gray-900 active:text-gray-200 block px-4 text-gray-900 rounded-[48px] hover:text-gray-500 py-2 "
+              >
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/all-plans"
+                className="active:bg-gray-900 active:text-gray-200 block px-4 text-gray-900 rounded-[48px] hover:text-gray-500 py-2"
+              >
+                All Plans
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/present"
+                className="active:bg-gray-900 active:text-gray-200 block px-4 text-gray-900 rounded-[48px] hover:text-gray-500 py-2"
+              >
+                Ready to present
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/todo"
+                className="active:bg-gray-900 active:text-gray-200 block px-4 text-gray-900 rounded-[48px] hover:text-gray-500 py-2"
+              >
+                To-do plans
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/create"
+                className="active:bg-gray-900 active:text-gray-200 block px-4 text-gray-900 rounded-[48px] hover:text-gray-500 py-2"
+              >
+                Create
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/patients"
+                className="active:bg-gray-900 active:text-gray-200 block px-4 text-gray-900 rounded-[48px] hover:text-gray-500 py-2"
+              >
+                Patients
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/settings"
+                className="active:bg-gray-900 active:text-gray-200 block px-4 text-gray-900 rounded-[48px] hover:text-gray-500 py-2"
+              >
+                Settings
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/help"
+                className="active:bg-gray-900 active:text-gray-200 block px-4 text-gray-900 rounded-[48px] hover:text-gray-500 py-2"
+              >
+                Help & Support
+              </NavLink>
+            </li>
+          </div>
           <li>
             <NavLink
-              to="/dashboard"
-              className={({ isActive }) =>
-                `block py-2.5 px-4 text-gray-900 ${
-                  isActive
-                    ? "bg-gray-900 text-gray-100 rounded-[48px]"
-                    : "hover:gray-800"
-                }`
-              }
+              to="/search"
+              className="py-2 my-4 active:text-gray-200 block px-4 text-gray-400 rounded-[48px] hover:text-gray-500 border border-gray-900"
             >
-              Dashboard
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/all-plans"
-              className={({ isActive }) =>
-                `block py-2.5 px-4 text-gray-900 ${
-                  isActive
-                    ? "bg-gray-900 text-gray-100 rounded-[48px]"
-                    : "hover:gray-800"
-                }`
-              }
-            >
-              All Plans
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/create"
-              className={({ isActive }) =>
-                `block py-2.5 px-4 text-gray-900 ${
-                  isActive
-                    ? "bg-gray-900 text-gray-100 rounded-[48px]"
-                    : "hover:gray-800"
-                }`
-              }
-            >
-              Create
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/settings"
-              className={({ isActive }) =>
-                `block py-2.5 px-4 text-gray-900 ${
-                  isActive
-                    ? "bg-gray-900 text-gray-100 rounded-[48px]"
-                    : "hover:gray-800"
-                }`
-              }
-            >
-              Settings
+              Search
             </NavLink>
           </li>
         </ul>
