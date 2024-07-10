901cea096ff0f2d498f69d0887b3f91e4ef34f36
terrxrmachine <so3.workspace@gmail.com>
2024-07-09 20:00:50 -0400

    хотфикс пдфки с исправленной разметкой

--- frontend/src/components/PatientReport.jsx
+++ frontend/src/components/PatientReport.jsx
@@ -29,7 +29,7 @@ const PatientReport = ({ patient }) => {
             className="w-full h-full object-cover"
           />
           {auth?.static_text?.slide3 && (
-            <div className="absolute bottom-12 left-12 text-white p-2">
+            <div className="absolute top-[350px] left-12 text-white p-2">
               <div className="bg-transparent text-white text-6xl border border-white rounded-3xl p-3 font-normal">
                 {auth.static_text.slide3}
               </div>
@@ -49,7 +49,7 @@ const PatientReport = ({ patient }) => {
             className="w-full h-full object-cover"
           />
           {auth?.static_text?.slide2 && (
-            <div className="absolute bottom-12 left-12 text-white p-2">
+            <div className="absolute top-[200px] left-[120px] text-white p-2">
               <div className="bg-transparent text-white text-6xl border border-white rounded-3xl p-3 font-normal">
                 {auth.static_text.slide2}
               </div>
@@ -120,7 +120,7 @@ const PatientReport = ({ patient }) => {
           />
 
           {auth?.static_text?.slide3 && (
-            <div className="absolute bottom-12 left-12 text-white p-2">
+            <div className="absolute top-[200px] right-12 text-white p-2">
               <div className="text-6xl py-5">Surgical & Implant Treatment</div>
               <div className="bg-transparent text-white text-2xl border border-white rounded-3xl p-3 font-normal">
                 {auth.static_text.slide3}
@@ -128,42 +128,42 @@ const PatientReport = ({ patient }) => {
             </div>
           )}
         </div>
-      </div>
 
-      {/* последний слайд */}
-      <div className="relative w-[16in] h-[9in] text-center items-center">
-        <img
-          src={patient?.photos[4]?.photo}
-          alt="Patient Photo 5"
-          className="w-full h-[4in] "
-        />
-        <div>
-          {auth?.static_text?.slide5 && (
-            <div className="flex text-black">
-              <div className="w-1/3">
-                <div className="text-3xl font-semibold py-5">
-                  Surgical & Implant Treatment
+        {/* последний слайд */}
+        <div className="relative w-[16in] h-[9in] text-center items-center">
+          <img
+            src={patient?.photos[4]?.photo}
+            alt="Patient Photo 5"
+            className="w-full h-[4in] "
+          />
+          <div>
+            {auth?.static_text?.slide5 && (
+              <div className="flex text-black">
+                <div className="w-1/3">
+                  <div className="text-3xl font-semibold py-5">
+                    Surgical & Implant Treatment
+                  </div>
+                  <div className="text-xl font-extralight rounded-3xl">
+                    {auth.static_text.slide5}
+                  </div>
                 </div>
-                <div className="text-xl font-extralight rounded-3xl">
-                  {auth.static_text.slide5}
+                <div className="w-1/3">
+                  <div className="text-3xl font-semibold py-5">Procedures</div>
+                  <div className="text-xl font-extralight rounded-3xl">
+                    {auth.static_text.slide5}
+                  </div>
+                </div>
+                <div className="w-1/3">
+                  <div className="text-3xl font-semibold py-5">
+                    Financial plan
+                  </div>
+                  <div className="text-xl font-extralight rounded-3xl">
+                    {auth.static_text.slide5}
+                  </div>
                 </div>
               </div>
-              <div className="w-1/3">
-                <div className="text-3xl font-semibold py-5">Procedures</div>
-                <div className="text-xl font-extralight rounded-3xl">
-                  {auth.static_text.slide5}
-                </div>
-              </div>
-              <div className="w-1/3">
-                <div className="text-3xl font-semibold py-5">
-                  Financial plan
-                </div>
-                <div className="text-xl font-extralight rounded-3xl">
-                  {auth.static_text.slide5}
-                </div>
-              </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
 
